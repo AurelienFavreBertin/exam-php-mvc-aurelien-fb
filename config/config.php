decf645496a28a197155de140c887df9a3c46a1d
Aurélien FAVRE- BERTIN <aurelien69fb@gmail.com>
2019-07-09 10:11:21 +0200

    init

--- config/config.php
+++ config/config.php
@@ -13,7 +13,7 @@ const DB_USER = 'root';
 const DB_PWD  = '';
 
 const WEBSITE_TITLE = "";
-const BASE_URL = "http://localhost/correction/";
+const BASE_URL = "http://localhost/cours/mvc-project-correction/";
 
 const CLASSES_SOURCES = [
     'src/controller',
